c9336a45977543304841d02fd74e445e28b4db90
Cristian López <60026545+cristianL0pez@users.noreply.github.com>
2022-06-22 22:48:16 -0400

    Update data
    data

--- api/dataclient.py
+++ api/dataclient.py
@@ -4,7 +4,7 @@ import requests
 url = 'https://randomuser.me/api/?results=20&inc=name,picture,dob,email,id,location,login,phone,registered' #url de la api
 data =  requests.get(url)                                     #request generica 
 users = data.json()['results']                                #tranformo data en json
-####################################################################
+##################################################################
 #creo la lista usuarios y vacia
 clients = []                                                
 for user in users:
